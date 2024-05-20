1fcd94dad7ab5d8eb1f7c91535f4a3fd7d173c05
Leejunhyeok03 <ly0609@naver.com>
2024-05-20 17:24:15 +0900

    fix: 피카츄 컴포넌트 안으로 로직 이동

--- assignments/02-pikachu/src/App.jsx
+++ assignments/02-pikachu/src/App.jsx
@@ -1,56 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useMemo } from "react";
 import "./App.css";
 import Grass from "./Grass";
 import Pikachu from "./Pikachu";
 
 function App() {
-  const grassComponents = Array.from({ length: 100 }, (_, index) => (
-    <Grass key={index} />
-  ));
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
-  const [rotateY, setRotateY] = useState(0);
-  const [translateY, setTranslateY] = useState(0);
-  const [eventCheck, setEventCheck] = useState(false);
-
-  const handleKeyUp = (e) => {
-    if (eventCheck) return;
-
-    setEventCheck(true);
-
-    switch (e.key) {
-      case "ArrowUp":
-        setY((prevY) => (prevY === 0 ? prevY : prevY - 10));
-        break;
-      case "ArrowDown":
-        setY((prevY) => (prevY === 90 ? prevY : prevY + 10));
-        break;
-      case "ArrowLeft":
-        setRotateY(180);
-        setX((prevX) => (prevX === 0 ? prevX : prevX - 10));
-        break;
-      case "ArrowRight":
-        setRotateY(0);
-        setX((prevX) => (prevX === 90 ? prevX : prevX + 10));
-        break;
-      case " ":
-        setTranslateY(-50);
-        setTimeout(() => setTranslateY(0), 300);
-        break;
-      default:
-        break;
-    }
-
-    setTimeout(() => setEventCheck(false), 100);
-  };
-
-  useEffect(() => {
-    document.addEventListener("keyup", handleKeyUp);
-
-    return () => {
-      document.removeEventListener("keyup", handleKeyUp);
-    };
-  }, [eventCheck]);
+  const grassComponents = useMemo(() => {
+    return Array.from({ length: 100 }, (_, index) => <Grass key={index} />);
+  }, []);
 
   return (
     <div
@@ -58,7 +14,7 @@ function App() {
       className="relative w-[100vh] h-[100vh] flex items-center flex-wrap bg-black m-auto"
     >
       {grassComponents}
-      <Pikachu x={x} y={y} rotateY={rotateY} translateY={translateY} />
+      <Pikachu />
     </div>
   );
 }
--- assignments/02-pikachu/src/Pikachu.jsx
+++ assignments/02-pikachu/src/Pikachu.jsx
@@ -1,8 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PigkachuImg from "./assets/image/pikachu.png";
 
-export default function Pikachu({ x, y, rotateY, translateY }) {
-  console.log(x);
+export default function Pikachu() {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [rotateY, setRotateY] = useState(0);
+  const [translateY, setTranslateY] = useState(0);
+  const handleKeyUp = (e) => {
+    switch (e.key) {
+      case "ArrowUp":
+        setY((prevY) => (prevY === 0 ? prevY : prevY - 10));
+        break;
+      case "ArrowDown":
+        setY((prevY) => (prevY === 90 ? prevY : prevY + 10));
+        break;
+      case "ArrowLeft":
+        setRotateY(180);
+        setX((prevX) => (prevX === 0 ? prevX : prevX - 10));
+        break;
+      case "ArrowRight":
+        setRotateY(0);
+        setX((prevX) => (prevX === 90 ? prevX : prevX + 10));
+        break;
+      case " ":
+        setTranslateY(-50);
+        setTimeout(() => setTranslateY(0), 300);
+        break;
+      default:
+        break;
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      document.removeEventListener("keyup", handleKeyUp);
+    };
+  }, []);
+
   return (
     <div
       className={`w-[10%] h-[10%] absolute ease-linear duration-200`}
